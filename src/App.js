1116d5c6bb6e8c6fa91f240076e03f88a4786a4e
mehtaReena <j.reena07@gmail.com>
2021-06-11 17:04:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import TableData from './TableData'
 import './styles.css';
+import Pagination from './Pagination';
 
 function App() {
   let [loading, setLoading] = useState(true)
   let [data, setData] = useState([])
   let [searcdata, setSearchData] = useState([])
-  let [option, setOption] = useState('')
+  let [option, setOption] = useState('');
+  let [page, setPage] = useState(5)
   let [searkKey, setSearckKey] = useState('')
   let inputRef = useRef();
   let selectRef = useRef();
+  let pageRef = useRef();
+  const [currentPage, setCurrentPage] = useState(1);
+
 
 
   const API_key = "Bearer iqi509189dxznal;,ggi";
@@ -34,39 +39,50 @@ function App() {
 
 
 
-    const changeHandel=()=>{
-      setOption(selectRef.current.value)
+  const changeHandel = () => {
+    setOption(selectRef.current.value)
+    setPage(pageRef.current.value)
+  }
+
+  const updateInput = async () => {
+    let input = inputRef.current.value
+    let option = selectRef.current.value;
+    let filtered = [];
+    if (option === 'City') {
+      filtered = data.filter(item => {
+        return item.city.toLowerCase().includes(input.toLowerCase())
+      })
+    }
+    else if (option === 'Name') {
+      filtered = data.filter(item => {
+        return item.name.toLowerCase().includes(input.toLowerCase())
+      })
+    }
+    else if (option === 'Geners') {
+      filtered = data.filter(item => {
+        return item.genre.toLowerCase().includes(input.toLowerCase())
+      })
     }
 
-    const updateInput = async () => {
-      let input =inputRef.current.value
-      let option =selectRef.current.value;
-      let filtered=[];
-      if(option==='City'){
-         filtered = data.filter(item => {
-          return item.city.toLowerCase().includes(input.toLowerCase())
-         })
-      }
-      else if (option==='Name'){
-        filtered = data.filter(item => {
-          return item.name.toLowerCase().includes(input.toLowerCase())
-         })
-        }
-         else if (option==='Geners'){
-           filtered = data.filter(item => {
-            return item.geners.toLowerCase().includes(input.toLowerCase())
-           })
-         }
 
+    setSearckKey(input);
+    setData(filtered);
+  }
 
-      setSearckKey(input);
-      setData(filtered);
-   }
-
-   useEffect(() => {
+  useEffect(() => {
     let intervalId = setInterval(() => { getData() }, 3000)
     return () => clearInterval(intervalId)
   }, [])
+
+
+
+
+  const indexOfLastPost = currentPage * page;
+  const indexOfFirstPost = indexOfLastPost - page;
+  const currentPosts = data.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
 
 
   return (
@@ -75,57 +91,72 @@ function App() {
 
         <h2>List of Restaurant</h2>
         {loading && <h3> Fetching...</h3>}
+        <div className="pagination" >
 
+            <select className ="pageBox"onChange={changeHandel} ref={pageRef}>
+              <option value="5" >5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+              <option value="20">20</option>
+            </select>
 
-        <div className="serachBox">
-          <select  onChange={changeHandel}  ref={selectRef}>
-            <option value="Name">Name</option>
-            <option value="City">City</option>
-            <option value="Geners">Geners</option>
-            <option value="State">State</option>
-          </select>
+            <Pagination
+              postsPerPage={page}
+              totalPosts={data.length}
+              paginate={paginate}
+            />
 
-          <input
-            type="text"
-            ref={inputRef}
-            placeholder="searching ..."
-            onChange={updateInput}
-          />
+          </div>
+
+          <div className="serachBox">
+            <select onChange={changeHandel} ref={selectRef}>
+              <option value="Name">Name</option>
+              <option value="City">City</option>
+              <option value="Geners">Geners</option>
+              <option value="State">State</option>
+            </select>
+
+            <input
+              type="text"
+              ref={inputRef}
+              placeholder="searching ..."
+              onChange={updateInput}
+            />
+
+          </div>
+
+          <table id='rest'>
+            <tr>
+              <th>Name</th>
+              <th>City</th>
+              <th>State</th>
+              <th>Phone Number</th>
+              <th>Geners</th>
+            </tr>
+            <tbody>
+              {
+                currentPosts.map((item) =>
+                  <TableData
+                    name={item.name}
+                    city={item.city}
+                    state={item.state}
+                    telephone={item.telephone}
+                    genre={item.genre}
+                  ></TableData>
+
+                )
+              }
+
+            </tbody>
+
+          </table>
 
         </div>
-
-        <table id='rest'>
-          <tr>
-            <th>Name</th>
-            <th>City</th>
-            <th>State</th>
-            <th>Phone Number</th>
-            <th>Geners</th>
-          </tr>
-          <tbody>
-            {
-              data.map((item) =>
-                <TableData
-                  name={item.name}
-                  city={item.city}
-                  state={item.state}
-                  telephone={item.telephone}
-                  genre={item.genre}
-                ></TableData>
-
-              )
-            }
-
-          </tbody>
-
-        </table>
-
       </div>
-    </div>
 
-  )
+      )
 
 }
 
 
-export default App;
+      export default App;
